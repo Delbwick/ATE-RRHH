9f215374b49f286e78270229039f388c49a4e211
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-07-16 12:52:14 +0200

    Update detalle_proyectos_rrhh.py

--- detalle_proyectos_rrhh.py
+++ detalle_proyectos_rrhh.py
@@ -416,6 +416,8 @@ valor_base = 33714.66  # euros
 
 # Calculamos el total específico (suponiendo que lo obtienes de alguna manera)
 #total_especifico = 500  # Este valor debería ser el total real de puntos específicos del proyecto
+# Calculamos el valor de puntos específicos del proyecto
+valor_punto_especifico_proyecto = (total_especifico * puntos_base) / valor_base
 
 # Aseguramos que valor_punto_especifico_proyecto no sea menor que min_value
 valor_punto_especifico_proyecto = max(valor_punto_especifico_proyecto, 1.0)
