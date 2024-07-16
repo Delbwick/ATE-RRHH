b10beb094ab5586b853ad4f2f7edafa84542e4af
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-07-16 09:58:52 +0200

    Update detalle_proyectos_rrhh.py

--- detalle_proyectos_rrhh.py
+++ detalle_proyectos_rrhh.py
@@ -364,7 +364,7 @@ if id_proyecto:
         total_especifico = result_df_especifico['puntos'].sum()
         st.write(f"Total puntos (Específico): {total_especifico}")
         total_puntos=total_especifico+total_destino
-        st.write(f"Total puntos: {total_especifico}+{total_destino}=" total_puntos)
+        st.write(f"Total puntos: {total_especifico}+{total_destino} = {total_puntos}")
         
     else:
         st.warning("No se encontraron datos para el ID de proyecto proporcionado en la consulta de complemento específico.")
