d727e2ff101f154ec2d9fa8ebf786ab22b593eb8
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-07-29 10:51:12 +0200

    Update Alta_proyecto_v2.py

--- Alta_proyecto_v2.py
+++ Alta_proyecto_v2.py
@@ -209,18 +209,6 @@ selected_factores = []
 for nombre_tabla, (nombre_completo, id_tabla) in PAGES_TABLES.items():
     if st.checkbox(nombre_tabla):
         selected_factores.append((nombre_completo, id_tabla))
-        
-# Mostrar selectboxes para seleccionar una fila por tabla
-selected_factores = {}
-for nombre_tabla, filas in PAGES_TABLES.items():
-    opciones = [fila[0] for fila in filas]
-    selected_opcion = st.selectbox(f"Seleccione una opción de {nombre_tabla}", ["Ninguno"] + opciones)
-    
-    if selected_opcion != "Ninguno":
-        # Encontrar la fila seleccionada
-        selected_fila = next(fila for fila in filas if fila[0] == selected_opcion)
-        selected_factores[nombre_tabla] = selected_fila
-
 
 
 # Mostrar los datos seleccionados
