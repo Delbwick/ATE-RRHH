c01fcc563ce98659e8610f98ac4c7681696ec051
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-08-21 20:07:03 +0200

    Update detalle_proyectos_rrhh.py

--- detalle_proyectos_rrhh.py
+++ detalle_proyectos_rrhh.py
@@ -211,7 +211,7 @@ for page_name in PAGES_TABLES:
     df = execute_query_for_page(page_name, id_proyecto_seleccionado)
     df,total_puntos_destino_1 = execute_query_for_page(page_name, id_proyecto_seleccionado)
 
-   if df is not None and not df.empty:  # Verificar si el DataFrame no está vacío
+    if df is not None and not df.empty:  # Verificar si el DataFrame no está vacío
     # Crear tres columnas con anchos 50%, 25%, 25%
         col1, col2, col3 = st.columns([2, 1, 1])
 
