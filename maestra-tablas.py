adb7f4b84a45978a3342a3eb45f3f26ac00c9915
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-09 19:39:23 +0200

    Update maestra-tablas.py

--- maestra-tablas.py
+++ maestra-tablas.py
@@ -452,9 +452,10 @@ if page in PAGES_TABLAS_NUEVAS:
 def get_table_names_with_prefix(prefix):
     query = f"""
         SELECT table_name 
-        FROM `ate-rrhh-2024.Ate_kaibot_2024.`
-        WHERE table_name LIKE '{prefix}%'
+        FROM `ate-rrhh-2024.Ate_kaibot_2024.INFORMATION_SCHEMA.TABLES`
+        WHERE table_name LIKE '%{prefix}%'
     """
+    
     result = client.query(query).result()
     return [row['table_name'] for row in result]
 
