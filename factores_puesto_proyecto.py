a4513c197542683f71bbd7e10bdd86d2cd7329ef
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-16 09:50:04 +0200

    Update factores_puesto_proyecto.py

--- factores_puesto_proyecto.py
+++ factores_puesto_proyecto.py
@@ -16,68 +16,22 @@ client = bigquery.Client(credentials=credentials)
 # Inicializa el cliente de BigQuery
 #client = bigquery.Client()
 
-# Función para seleccionar los proyectos desde BigQuery
-def get_proyectos():
-    query = """
-        SELECT id_projecto, nombre
-        FROM `ate-rrhh-2024.Ate_kaibot_2024.proyecto`
-    """
-    query_job = client.query(query)
-    results = query_job.result()
-    proyectos = [{'id': row.id_projecto, 'nombre': row.nombre} for row in results]
-    return proyectos
-
-# Mostrar el encabezado y línea separadora
-st.markdown("<h2>Selecciona el proyecto que quieres calcular</h2>", unsafe_allow_html=True)
-st.markdown("<div class='wide-line'></div>", unsafe_allow_html=True)
-
-# Obtener lista de proyectos
-proyectos = get_proyectos()
-
-# Extraer solo los nombres de los proyectos para el selectbox
-proyectos_nombres = [proyecto['nombre'] for proyecto in proyectos]
-
-# Mostrar el cuadro de selección de proyectos
-index_seleccionado = st.selectbox("Selecciona un proyecto", proyectos_nombres)
-
-# Obtener el ID del proyecto seleccionado
-id_proyecto_seleccionado = None
-for proyecto in proyectos:
-    if proyecto['nombre'] == index_seleccionado:
-        id_proyecto_seleccionado = proyecto['id']
-        break
-
-# Mostrar el ID seleccionado (solo para propósitos de verificación)
-if id_proyecto_seleccionado is not None:
-    st.write(f"ID del proyecto seleccionado: {id_proyecto_seleccionado}")
-else:
-    st.write("Selecciona un proyecto para ver su ID")
-
-# Puedes usar 'id_proyecto_seleccionado' en tu lógica posterior según sea necesario
-
-
-
 # Función para obtener puestos
 def get_puestos():
-    query = "SELECT id_puesto, descripcion FROM `ate-rrhh-2024.Ate_kaibot_2024.puestos`"
+    query = "SELECT descripcion FROM `ate-rrhh-2024.Ate_kaibot_2024.puestos`"
     query_job = client.query(query)
-    puestos_df = query_job.result().to_dataframe()  # Convertimos a DataFrame
-    return puestos_df
+    results = query_job.result()
+    return [row.descripcion for row in results]
 
 # Función para obtener factores seleccionados
-def get_factores_seleccionados(id_proyecto,selected_puestos_id):
+def get_factores_seleccionados(id_proyecto):
     query = f"""
-    SELECT complementos_especificos, complementos_destino
+    SELECT DISTINCT complementos_especificos, complementos_destino
     FROM `ate-rrhh-2024.Ate_kaibot_2024.factores_seleccionados_x_puesto_x_proyecto`
-    WHERE id_proyecto = {id_proyecto} AND id_puesto={selected_puestos_id}
-    GROUP BY complementos_especificos, complementos_destino
+    WHERE id_proyecto = {id_proyecto}
     """
     query_job = client.query(query)
     df = query_job.result().to_dataframe()
-
-     # Eliminar filas duplicadas, manteniendo solo combinaciones únicas
-    #df = df.drop_duplicates(subset=['complementos_especificos', 'complementos_destino'])
-    
     return df
 
 # Función para obtener los datos de una tabla específica
@@ -99,27 +53,16 @@ def insertar_datos(table_id, rows_to_insert):
 st.title('Gestión de Proyectos y Factores')
 
 # Selección de Puestos
-# Obtener los puestos disponibles
-puestos_df = get_puestos()
-
-# Crear un diccionario que mapee descripcion -> id_puesto
-puestos_dict = dict(zip(puestos_df['descripcion'], puestos_df['id_puesto']))
-
-# Mostrar el multiselect para seleccionar los puestos (basado en descripcion)
 st.markdown("<h2>Selecciona los Puestos de Trabajo</h2>", unsafe_allow_html=True)
-selected_puestos_desc = st.multiselect("Selecciona los puestos", puestos_df['descripcion'])
+puestos = get_puestos()
+selected_puestos = st.multiselect("Selecciona los puestos", puestos)
 
-# Obtener los id_puesto correspondientes a las descripciones seleccionadas
-selected_puestos_id = [puestos_dict[desc] for desc in selected_puestos_desc]
-
-# Mostrar los id_puesto seleccionados
-st.write(f"ID de Puestos seleccionados: {selected_puestos_id}")
 # Selección de Proyecto
-id_proyecto = st.number_input('ID de Proyecto', min_value=1,value=id_proyecto_seleccionado, step=1)
+id_proyecto = st.number_input('ID de Proyecto', min_value=1, step=1)
 
 # Mostrar factores seleccionados para el proyecto
 if id_proyecto:
-    factores_df = get_factores_seleccionados(id_proyecto,selected_puestos_id)
+    factores_df = get_factores_seleccionados(id_proyecto)
 
     if not factores_df.empty:
         st.write("Factores Seleccionados para el Proyecto")
