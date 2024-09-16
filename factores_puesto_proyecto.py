cf6a2e11f5a1a8a8c29cdacabd082aa976040f31
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-16 10:55:50 +0200

    Update factores_puesto_proyecto.py

--- factores_puesto_proyecto.py
+++ factores_puesto_proyecto.py
@@ -117,14 +117,33 @@ def insertar_datos(table_id, rows_to_insert):
 # Aplicación Streamlit
 st.title('Gestión de Proyectos y Factores')
 
-# Selección de Puestos
-
 # Selección de Proyecto
-id_proyecto = st.number_input('ID de Proyecto', min_value=1,value=id_proyecto_seleccionado, step=1)
+st.markdown("<h2>Selecciona el proyecto que quieres calcular</h2>", unsafe_allow_html=True)
+proyectos = get_proyectos()
+proyectos_nombres = [proyecto['nombre'] for proyecto in proyectos]
+index_seleccionado = st.selectbox("Selecciona un proyecto", proyectos_nombres)
+id_proyecto_seleccionado = next(proyecto['id'] for proyecto in proyectos if proyecto['nombre'] == index_seleccionado)
 
-st.markdown("<h2>Selecciona los Puestos de Trabajo</h2>", unsafe_allow_html=True)
-puestos = get_puestos(id_proyecto)
-selected_puestos = st.multiselect("Selecciona los puestos", puestos)
+# Obtener y seleccionar Puestos
+puestos = get_puestos(id_proyecto_seleccionado)
+puestos_descripciones = [puesto['descripcion'] for puesto in puestos]
+selected_puesto = st.selectbox("Selecciona un puesto", puestos_descripciones)
+
+# Obtener el ID del puesto seleccionado
+id_puesto_seleccionado = next((puesto['id'] for puesto in puestos if puesto['descripcion'] == selected_puesto), None)
+
+# Mostrar factores seleccionados para el proyecto y puesto
+if id_proyecto_seleccionado and id_puesto_seleccionado:
+    factores_df = get_factores_seleccionados(id_proyecto_seleccionado, id_puesto_seleccionado)
+
+    if not factores_df.empty:
+        st.write("Factores Seleccionados para el Proyecto y Puesto")
+        st.dataframe(factores_df)
+    else:
+        st.write("No se encontraron factores para la selección.")
+else:
+    st.write("Selecciona un proyecto y un puesto para ver los factores.")
+
 
 
 # Mostrar factores seleccionados para el proyecto
