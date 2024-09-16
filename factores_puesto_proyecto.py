d2d8714599f800a9fcac67001fa2b865454e3cfc
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-16 11:14:43 +0200

    Update factores_puesto_proyecto.py

--- factores_puesto_proyecto.py
+++ factores_puesto_proyecto.py
@@ -16,7 +16,6 @@ client = bigquery.Client(credentials=credentials)
 # Inicializa el cliente de BigQuery
 #client = bigquery.Client()
 
-# Función para seleccionar los proyectos desde BigQuery
 def get_proyectos():
     query = """
         SELECT id_projecto, nombre
@@ -27,37 +26,6 @@ def get_proyectos():
     proyectos = [{'id': row.id_projecto, 'nombre': row.nombre} for row in results]
     return proyectos
 
-# Mostrar el encabezado y línea separadora
-st.markdown("<h2>Selecciona el proyecto que quieres calcular</h2>", unsafe_allow_html=True)
-st.markdown("<div class='wide-line'></div>", unsafe_allow_html=True)
-
-# Obtener lista de proyectos
-proyectos = get_proyectos()
-
-# Extraer solo los nombres de los proyectos para el selectbox
-proyectos_nombres = [proyecto['nombre'] for proyecto in proyectos]
-
-# Mostrar el cuadro de selección de proyectos
-index_seleccionado = st.selectbox("Selecciona un proyecto", proyectos_nombres)
-
-# Obtener el ID del proyecto seleccionado
-id_proyecto_seleccionado = None
-for proyecto in proyectos:
-    if proyecto['nombre'] == index_seleccionado:
-        id_proyecto_seleccionado = proyecto['id']
-        break
-
-# Mostrar el ID seleccionado (solo para propósitos de verificación)
-if id_proyecto_seleccionado is not None:
-    st.write(f"ID del proyecto seleccionado: {id_proyecto_seleccionado}")
-else:
-    st.write("Selecciona un proyecto para ver su ID")
-
-# Puedes usar 'id_proyecto_seleccionado' en tu lógica posterior según sea necesario
-
-
-
-# Función para obtener puestos
 def get_puestos(id_proyecto):
     # Obtener los IDs de los puestos relacionados con el proyecto
     query_ids = f"""
@@ -83,30 +51,23 @@ def get_puestos(id_proyecto):
     else:
         return []
 
-
-# Función para obtener factores seleccionados
-def get_factores_seleccionados(id_proyecto):
+def get_factores_seleccionados(id_proyecto, id_puesto):
     query = f"""
-    SELECT complementos_especificos, complementos_destino
+    SELECT DISTINCT complementos_especificos, complementos_destino
     FROM `ate-rrhh-2024.Ate_kaibot_2024.factores_seleccionados_x_puesto_x_proyecto`
-    WHERE id_proyecto = {id_proyecto}
+    WHERE id_proyecto = {id_proyecto} AND id_puesto = {id_puesto}
     """
     query_job = client.query(query)
     df = query_job.result().to_dataframe()
 
-     # Eliminar filas duplicadas, manteniendo solo combinaciones únicas
-    df = df.drop_duplicates(subset=['complementos_especificos', 'complementos_destino'])
-    
     return df
 
-# Función para obtener los datos de una tabla específica
 def obtener_datos_tabla(tabla):
     query = f"SELECT * FROM `{tabla}` LIMIT 100"
     query_job = client.query(query)
     df = query_job.result().to_dataframe()
     return df
 
-# Función para insertar datos en BigQuery
 def insertar_datos(table_id, rows_to_insert):
     errors = client.insert_rows_json(table_id, rows_to_insert)
     if errors:
@@ -117,44 +78,50 @@ def insertar_datos(table_id, rows_to_insert):
 # Aplicación Streamlit
 st.title('Gestión de Proyectos y Factores')
 
-# Selección de Puestos
-
 # Selección de Proyecto
-id_proyecto = st.number_input('ID de Proyecto', min_value=1,value=id_proyecto_seleccionado, step=1)
+st.markdown("<h2>Selecciona el proyecto que quieres calcular</h2>", unsafe_allow_html=True)
+proyectos = get_proyectos()
+proyectos_nombres = [proyecto['nombre'] for proyecto in proyectos]
+index_seleccionado = st.selectbox("Selecciona un proyecto", proyectos_nombres)
+id_proyecto_seleccionado = next(proyecto['id'] for proyecto in proyectos if proyecto['nombre'] == index_seleccionado)
 
+# Selección de Puestos
 st.markdown("<h2>Selecciona los Puestos de Trabajo</h2>", unsafe_allow_html=True)
-puestos = get_puestos(id_proyecto)
-selected_puestos = st.multiselect("Selecciona los puestos", puestos)
-
+puestos = get_puestos(id_proyecto_seleccionado)
+puestos_descripciones = [puesto['descripcion'] for puesto in puestos]
+selected_puestos = st.multiselect("Selecciona los puestos", puestos_descripciones)
 
 # Mostrar factores seleccionados para el proyecto
-if id_proyecto:
-    factores_df = get_factores_seleccionados(id_proyecto)
+if id_proyecto_seleccionado and selected_puestos:
+    for descripcion in selected_puestos:
+        id_puesto = next(puesto['id'] for puesto in puestos if puesto['descripcion'] == descripcion)
 
-    if not factores_df.empty:
-        st.write("Factores Seleccionados para el Proyecto")
-        st.dataframe(factores_df)
-        
-        for index, row in factores_df.iterrows():
-            # Obtener los datos de las tablas basadas en los factores
-            tabla_especificos = row['complementos_especificos']
-            tabla_destino = row['complementos_destino']
+        factores_df = get_factores_seleccionados(id_proyecto_seleccionado, id_puesto)
 
-            st.markdown(f"### Factores Específicos: {tabla_especificos}")
-            df_especificos = obtener_datos_tabla(tabla_especificos)
-            if not df_especificos.empty:
-                opciones = [f"{row['descripcion']} ({row['letra']})" for index, row in df_especificos.iterrows()]
-                seleccion_especifico = st.radio(f"Seleccione un valor para {tabla_especificos.split('.')[-1]}:", opciones, key=f"especifico_{index}")
-                if seleccion_especifico:
-                    selected_value_especifico = df_especificos.loc[opciones.index(seleccion_especifico), df_especificos.columns[0]]
+        if not factores_df.empty:
+            st.write(f"Factores Seleccionados para el Proyecto {id_proyecto_seleccionado} y Puesto {id_puesto}")
+            st.dataframe(factores_df)
+            
+            for index, row in factores_df.iterrows():
+                # Obtener los datos de las tablas basadas en los factores
+                tabla_especificos = row['complementos_especificos']
+                tabla_destino = row['complementos_destino']
+
+                st.markdown(f"### Factores Específicos: {tabla_especificos}")
+                df_especificos = obtener_datos_tabla(tabla_especificos)
+                if not df_especificos.empty:
+                    opciones_especificos = [f"{row['descripcion']} ({row['letra']})" for index, row in df_especificos.iterrows()]
+                    seleccion_especifico = st.radio(f"Seleccione un valor para {tabla_especificos.split('.')[-1]}:", opciones_especificos, key=f"especifico_{index}")
+                    if seleccion_especifico:
+                        selected_value_especifico = df_especificos.loc[opciones_especificos.index(seleccion_especifico), df_especificos.columns[0]]
                     
-            st.markdown(f"### Factores de Destino: {tabla_destino}")
-            df_destino = obtener_datos_tabla(tabla_destino)
-            if not df_destino.empty:
-                opciones = [f"{row['descripcion']} ({row['letra']})" for index, row in df_destino.iterrows()]
-                seleccion_destino = st.radio(f"Seleccione un valor para {tabla_destino.split('.')[-1]}:", opciones, key=f"destino_{index}")
-                if seleccion_destino:
-                    selected_value_destino = df_destino.loc[opciones.index(seleccion_destino), df_destino.columns[0]]
+                st.markdown(f"### Factores de Destino: {tabla_destino}")
+                df_destino = obtener_datos_tabla(tabla_destino)
+                if not df_destino.empty:
+                    opciones_destino = [f"{row['descripcion']} ({row['letra']})" for index, row in df_destino.iterrows()]
+                    seleccion_destino = st.radio(f"Seleccione un valor para {tabla_destino.split('.')[-1]}:", opciones_destino, key=f"destino_{index}")
+                    if seleccion_destino:
+                        selected_value_destino = df_destino.loc[opciones_destino.index(seleccion_destino), df_destino.columns[0]]
 
             # Aquí podrías agregar lógica para almacenar los valores seleccionados si es necesario
 
@@ -162,14 +129,10 @@ if id_proyecto:
 if st.button('Guardar Datos'):
     rows_to_insert = []
     for descripcion in selected_puestos:
-        query = f"""
-        SELECT id_puesto FROM `ate-rrhh-2024.Ate_kaibot_2024.puestos` WHERE descripcion = '{descripcion}'
-        """
-        query_job = client.query(query)
-        id_puesto = query_job.result().to_dataframe()['id_puesto'].iloc[0]
+        id_puesto = next(puesto['id'] for puesto in puestos if puesto['descripcion'] == descripcion)
         
         row = {
-            'id_proyecto': id_proyecto,
+            'id_proyecto': id_proyecto_seleccionado,
             'id_puesto': id_puesto,
             # Añadir aquí otros campos necesarios como los valores seleccionados
         }
