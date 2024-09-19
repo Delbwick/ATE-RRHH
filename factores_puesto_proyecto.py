5f25a1fe6442b91a6eb1b005131671ef6b46a43b
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-19 17:33:16 +0200

    Update factores_puesto_proyecto.py

--- factores_puesto_proyecto.py
+++ factores_puesto_proyecto.py
@@ -80,51 +80,74 @@ if id_proyecto_seleccionado and selected_puestos:
     st.markdown(f"### Factores Seleccionados para el Proyecto {id_proyecto_seleccionado}")
 
     for descripcion in selected_puestos:
-        id_puesto = puestos_df.query(f"descripcion == '{descripcion}'")['id_puesto'].values[0]
-        factores_df = get_factores_seleccionados(id_proyecto_seleccionado, id_puesto)
+    id_puesto = puestos_df.query(f"descripcion == '{descripcion}'")['id_puesto'].values[0]
+    factores_df = get_factores_seleccionados(id_proyecto_seleccionado, id_puesto)
 
-        if not factores_df.empty:
-            st.write(f"Factores para el Puesto {id_puesto} ({descripcion})")
+    if not factores_df.empty:
+        st.write(f"Factores para el Puesto {id_puesto} ({descripcion})")
 
-            for index, row in factores_df.iterrows():
-                tabla_especificos = row['complementos_especificos']
-                tabla_destino = row['complementos_destino']
+        for index, row in factores_df.iterrows():
+            tabla_especificos = row['complementos_especificos']
+            tabla_destino = row['complementos_destino']
 
-                if tabla_especificos != 'No disponible':
-                    st.subheader(f"Factores Específicos: {tabla_especificos}")
-                    df_especificos = obtener_datos_tabla(tabla_especificos)
-                    if not df_especificos.empty:
-                        st.write("Tabla de Factores Específicos")
-                        st.dataframe(df_especificos)
+            # Factores Específicos
+            if tabla_especificos != 'No disponible':
+                st.subheader(f"Factores Específicos: {tabla_especificos}")
+                df_especificos = obtener_datos_tabla(tabla_especificos)
+                if not df_especificos.empty:
+                    st.write("Tabla de Factores Específicos")
+                    st.dataframe(df_especificos)
+                    
+                    # Selección de un valor específico
+                    opciones_especificos = df_especificos.apply(lambda r: f"{r['letra']} - {r['descripcion']}", axis=1).tolist()
+                    seleccion_especifico = st.selectbox(f"Selecciona un valor para {tabla_especificos.split('.')[-1]}:", opciones_especificos, key=f"especifico_{index}")
+                    if seleccion_especifico:
+                        selected_letra, selected_descripcion = seleccion_especifico.split(" - ")
+                        puntos = df_especificos.query(f"letra == '{selected_letra}'")['puntos'].values[0]
                         
-                        opciones_especificos = df_especificos.apply(lambda r: f"{r['letra']} - {r['descripcion']}", axis=1).tolist()
-                        seleccion_especifico = st.selectbox(f"Selecciona un valor para {tabla_especificos.split('.')[-1]}:", opciones_especificos, key=f"especifico_{index}")
-                        if seleccion_especifico:
-                            selected_letra, selected_descripcion = seleccion_especifico.split(" - ")
-                            st.write(f"Seleccionaste la letra: {selected_letra} y la descripción: {selected_descripcion}")
-                            puntos = df_especificos.query(f"letra == '{selected_letra}'")['puntos'].values[0]
-                            selecciones_especificos.append({'Puesto': descripcion, 'Letra': selected_letra, 'Descripción': selected_descripcion, 'Puntos': puntos})
-                    else:
-                        st.write(f"No se encontraron datos para la tabla de factores específicos {tabla_especificos}.")
-                
-                if tabla_destino != 'No disponible':
-                    st.subheader(f"Factores de Destino: {tabla_destino}")
-                    df_destino = obtener_datos_tabla(tabla_destino)
-                    if not df_destino.empty:
-                        st.write("Tabla de Factores de Destino")
-                        st.dataframe(df_destino)
+                        # Input para porcentaje
+                        porcentaje_especifico = st.number_input(f"Introduce el porcentaje para {selected_descripcion}:", min_value=0.0, max_value=100.0, value=100.0, step=1.0, key=f'porcentaje_especifico_{index}')
                         
-                        opciones_destino = df_destino.apply(lambda r: f"{r['letra']} - {r['descripcion']}", axis=1).tolist()
-                        seleccion_destino = st.selectbox(f"Selecciona un valor para {tabla_destino.split('.')[-1]}:", opciones_destino, key=f"destino_{index}")
-                        if seleccion_destino:
-                            selected_letra_destino, selected_descripcion_destino = seleccion_destino.split(" - ")
-                            st.write(f"Seleccionaste la letra: {selected_letra_destino} y la descripción: {selected_descripcion_destino}")
-                            puntos_destino = df_destino.query(f"letra == '{selected_letra_destino}'")['puntos'].values[0]
-                            selecciones_destino.append({'Puesto': descripcion, 'Letra': selected_letra_destino, 'Descripción': selected_descripcion_destino, 'Puntos': puntos_destino})
-                    else:
-                        st.write(f"No se encontraron datos para la tabla de factores de destino {tabla_destino}.")
-        else:
-            st.write(f"No se encontraron factores para el Puesto {id_puesto} ({descripcion}).")
+                        # Calcular puntos ajustados
+                        puntos_ajustados = puntos * (porcentaje_especifico / 100)
+                        
+                        # Mostrar resultado
+                        st.write(f"Seleccionaste la letra: {selected_letra} y la descripción: {selected_descripcion}")
+                        st.write(f"Puntos originales: {puntos}")
+                        st.write(f"Puntos ajustados (con {porcentaje_especifico}%): {puntos_ajustados:.2f}")
+                        selecciones_especificos.append({'Puesto': descripcion, 'Letra': selected_letra, 'Descripción': selected_descripcion, 'Puntos': puntos_ajustados})
+                else:
+                    st.write(f"No se encontraron datos para la tabla de factores específicos {tabla_especificos}.")
+            
+            # Factores de Destino
+            if tabla_destino != 'No disponible':
+                st.subheader(f"Factores de Destino: {tabla_destino}")
+                df_destino = obtener_datos_tabla(tabla_destino)
+                if not df_destino.empty:
+                    st.write("Tabla de Factores de Destino")
+                    st.dataframe(df_destino)
+                    
+                    # Selección de un valor destino
+                    opciones_destino = df_destino.apply(lambda r: f"{r['letra']} - {r['descripcion']}", axis=1).tolist()
+                    seleccion_destino = st.selectbox(f"Selecciona un valor para {tabla_destino.split('.')[-1]}:", opciones_destino, key=f"destino_{index}")
+                    if seleccion_destino:
+                        selected_letra_destino, selected_descripcion_destino = seleccion_destino.split(" - ")
+                        puntos_destino = df_destino.query(f"letra == '{selected_letra_destino}'")['puntos'].values[0]
+                        
+                        # Input para porcentaje
+                        porcentaje_destino = st.number_input(f"Introduce el porcentaje para {selected_descripcion_destino}:", min_value=0.0, max_value=100.0, value=100.0, step=1.0, key=f'porcentaje_destino_{index}')
+                        
+                        # Calcular puntos ajustados
+                        puntos_ajustados_destino = puntos_destino * (porcentaje_destino / 100)
+                        
+                        # Mostrar resultado
+                        st.write(f"Seleccionaste la letra: {selected_letra_destino} y la descripción: {selected_descripcion_destino}")
+                        st.write(f"Puntos originales: {puntos_destino}")
+                        st.write(f"Puntos ajustados (con {porcentaje_destino}%): {puntos_ajustados_destino:.2f}")
+                        selecciones_destino.append({'Puesto': descripcion, 'Letra': selected_letra_destino, 'Descripción': selected_descripcion_destino, 'Puntos': puntos_ajustados_destino})
+                else:
+                    st.write(f"No se encontraron datos para la tabla de factores de destino {tabla_destino}.")
+
 
     # Mostrar la tabla de resumen final
     for descripcion in selected_puestos:
