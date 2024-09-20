bbc72bf1dd9a005e3db8c346e92ae4ccf1757ccc
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-09-20 09:11:18 +0200

    Update factores_puesto_proyecto.py

--- factores_puesto_proyecto.py
+++ factores_puesto_proyecto.py
@@ -3,6 +3,82 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
 
+# Configurar la página de Streamlit
+st.set_page_config(page_title="RRHH Cálculo de Puestos por Proyecto y por puesto ", page_icon="🙇")
+st.title("¡Bienvenido a RRHH! ")
+st.header("¡Calcula los Salarios Por Poryecto!")
+
+# HTML personalizado para el encabezado
+header_html = """
+    <style>
+        .header-container {
+            background-color: #2596be; /* Color de fondo */
+            padding: 20px;
+            border-radius: 10px;
+            text-align: center;
+        }
+        .logo {
+            max-width: 150px;
+            margin-bottom: 10px;
+        }
+        .wide-line {
+        width: 100%;
+        height: 2px;
+        background-color: #333333;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    h1 {
+        font-family: 'Arial', sans-serif;
+        font-size: 17pt;
+        text-align: left;
+        color: #333333;
+    }
+    h2 {
+        font-family: 'Arial', sans-serif;
+        font-size: 17pt;
+        text-align: left;
+        color: #333333;
+    }
+    h3 {
+        font-family: 'Arial', sans-serif;
+        font-size: 14pt;
+        text-align: center;
+        color: #333333;
+    }
+    .cell {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+        background-color: #f9f9f9;
+        margin-bottom: 20px; /* Margen inferior para toda la "tabla" */
+    }
+    .header-cell {
+        background-color: #e0e0e0;
+        font-weight: bold;
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+       
+    }
+    .dataframe-cell {
+        overflow-x: auto;  /* Habilita scroll horizontal */
+        overflow-y: auto;  /* Habilita scroll vertical */
+        max-width: 100%;   /* Limita el ancho al 100% del contenedor */
+        max-height: 200px; /* Limita la altura a 300px y habilita scroll */
+    }
+    </style>
+"""
+
+# Agregar el HTML personalizado al encabezado
+st.markdown(header_html, unsafe_allow_html=True)
+
+# Agregar la imagen (logo) y el texto al encabezado
+st.markdown('<div class="header-container"><img class="logo" src="https://www.rrhhdelnorte.es/-_-/res/702f8fd0-46a5-4f0d-9c65-afb737164745/images/files/702f8fd0-46a5-4f0d-9c65-afb737164745/e0e4dc73-78c2-4413-b62c-250cbeea83fa/683-683/3b3822cd156fd081c427cc6b35617e4031b98c63" alt="Logo"></div>', unsafe_allow_html=True)
+#st.write("Detalle de proyectos")
+
+
+
 # Crear API client para BigQuery
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
