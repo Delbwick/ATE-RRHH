b8f62a0467c486671aba4b90ae657d0bb777acea
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-10-03 09:31:53 +0200

    Update alta_proyecto_v3.py

--- alta_proyecto_v3.py
+++ alta_proyecto_v3.py
@@ -242,7 +242,7 @@ dataset_id = 'Ate_kaibot_2024'
 # Modificar la consulta para excluir tablas con nombres concretos
 query = f"""
     SELECT table_name
-    FROM `{project_id}.{dataset_id}.INFORMATION_SCHEMA.TABLE_OPTIONS`
+    FROM `ate-rrhh-2024.Ate_kaibot_2024.INFORMATION_SCHEMA.TABLE_OPTIONS`
     WHERE option_name = 'labels'
     AND option_value LIKE '%"destino"%'
 """
