ef8849c9281dcdb283dfb6ceb46bd509ea2f8a61
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-10-16 12:18:17 +0200

    Update alta_proyecto_v3.py

--- alta_proyecto_v3.py
+++ alta_proyecto_v3.py
@@ -117,11 +117,28 @@ else:
     st.warning("Por favor, selecciona al menos un puesto para continuar.")
 
 #Funcion para ibcluir algun puesto nuevo
+#Primero vamos a obtener el nuevo id
+
 # Función para insertar un nuevo puesto en BigQuery
 def add_puesto(nuevo_puesto):
+    #Primero el id_puesto
+    # Consulta para obtener el último ID de proyecto
+    query_max_id_puestos = """
+        SELECT MAX(id_puesto) FROM `ate-rrhh-2024.Ate_kaibot_2024.puestos`
+        """
+        query_job_max_id_puestos = client.query(query_max_id_puestos)
+        max_id_result_puesto = query_job_max_id_puestos.result()
+
+        max_id_puesto = 0
+        for row in max_id_result_puesto:
+            max_id_puesto = row[0]
+
+        # Incrementar el máximo ID en 1 para obtener el nuevo ID de proyecto
+        new_id_puesto = max_id_puesto + 1 if max_id_puesto is not None else 1
+    #
     query = f"""
-        INSERT INTO `ate-rrhh-2024.Ate_kaibot_2024.puestos` (descripcion)
-        VALUES ('{nuevo_puesto}')
+        INSERT INTO `ate-rrhh-2024.Ate_kaibot_2024.puestos` (id_puesto,descripcion)
+        VALUES ({id_puesto,}'{nuevo_puesto}')
     """
     query_job = client.query(query)
     query_job.result()  # Esperar a que la inserción se complete
