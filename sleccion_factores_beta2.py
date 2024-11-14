6b6718ef74a6bd5b34c4676c3ba262f8bad2df7d
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-11-14 09:44:01 +0100

    Update sleccion_factores_beta2.py

--- sleccion_factores_beta2.py
+++ sleccion_factores_beta2.py
@@ -264,9 +264,9 @@ for nombre_tabla, (nombre_completo, id_tabla) in PAGES_TABLES.items():
                 puntos_ajustados_destino = puntos_destino * (porcentaje_destino / 100)
 
                 # Mostrar resultados en una nueva línea
-                st.markdown("<h4>Resultados de la Selección</h4>", unsafe_allow_html=True)
-                st.write(f"Puntos originales: {puntos_destino}")
-                st.write(f"Puntos ajustados (con {porcentaje_destino}%): {puntos_ajustados_destino:.2f}")
+                #st.markdown("<h4>Resultados de la Selección</h4>", unsafe_allow_html=True)
+                #st.write(f"Puntos originales: {puntos_destino}")
+                #st.write(f"Puntos ajustados (con {porcentaje_destino}%): {puntos_ajustados_destino:.2f}")
 
                 # Guardar en la lista de selecciones
                 selecciones_destino.append({
