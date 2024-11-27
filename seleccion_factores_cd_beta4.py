62bf5562ecbf865abc2725a7f916ca66ba09f6cf
Delbwick <167512941+Delbwick@users.noreply.github.com>
2024-11-27 18:00:09 +0100

    Update seleccion_factores_cd_beta4.py

--- seleccion_factores_cd_beta4.py
+++ seleccion_factores_cd_beta4.py
@@ -406,9 +406,9 @@ if st.button("Guardar selecciones"):
 
 # Crear un botón
 st.markdown("""
-    <a href="https://ate-rrhh-jwinmwitfd8gsoc4va9cjc.streamlit.app/?id_proyecto={id_proyecto_url}" target="_blank">
+    <a href="https://ate-rrhh-gvym68tf8xp2pdhq6dy8xj.streamlit.app//?id_proyecto={id_proyecto_url}" target="_blank">
         <button style="background-color:Green;padding:10px;border-radius:5px;color:white;border:none;">
-            Ir a Menu
+            Ir a Seleccion de Compelementos Especificos
         </button>
     </a>
     """, unsafe_allow_html=True)
